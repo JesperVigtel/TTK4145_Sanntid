e57f767cd2600e8d1a65f92556e733c0d615e0ea
Andreas Frafjord-Moe <andrfra@stud.ntnu.no>
2026-02-25 16:31:29 +0100

    la til en chan i local

--- elevator/internal/localControll/localControll.go
+++ elevator/internal/localControll/localControll.go
@@ -29,12 +29,14 @@ func localControl(
 		recoveryTickChan  = make(chan bool, 1) 
 		obstructionChan   = make(chan bool, config.ChannelBufferSize)
 		buttonPressChan   = make(chan types.OrderEvent, config.ChannelBufferSize)
+		StopPressChan		= make(chan bool, 1)
 		obstruction  bool
 	)
 
 	go hardware.PollFloorSensor(floorChan)
 	go hardware.PollObstructionSwitch(obstructionChan)
 	go hardware.PollButtons(buttonPressChan)
+	go hardware.PollStopButton(StopPressChan)
 
 	go timer.Timer(doorOpenChan, motorActiveChan, recoveryEnableChan,  doorClosedChan, motorInactiveChan, recoveryTickChan)
 
@@ -49,7 +51,6 @@ func localControl(
 			elevator.CurrentFloor = floor
 			elevator.ActiveStatus = true
 			elevator.Behaviour = types.ElevatorMoving
-			//sjekk om den 
 
 		case orders := <-newOrder:
 			elevator.Request = orders
