f98f3c456a0b26463bfa6124903c54ec1ddfaba8
Anton Oegren <anton.oegren@gmail.com>
2026-03-03 11:50:46 +0100

    Reorganisering av nettverksfilstruktur

--- elevator/internal/network/peers.go
+++
@@ -1,6 +0,0 @@
-package network
-
-// Håndterer "peer discovery" (hvem er online/offline via UDP).
-// Lytter på nettverket for å oppdage andre heis-noder som blir tilgjengelige eller forsvinner.
-
-// TODO: Implementer peer discovery-logikk
