fa665854b646a1a98e097afd8aa77749038cf9e9
Espen <ebentdal@gmail.com>
2026-03-09 15:01:02 +0100

    something

--- elevator/internal/localControl/localControl.go
+++ elevator/internal/localControl/localControl.go
@@ -64,16 +64,11 @@ func Run(
 				sendElevatorUpdate(elevatorEvents, elevator, obstruction, [config.NFloors][config.NButtons]bool{}, nil)
 			}
 
+
 		case orders := <-newOrder:
 			fmt.Println("[EVENT] Received new order table")
 			elevator.LocalOrders = orders
-			sendLightUpdate(localLightsChan, elevator, elevator.Behaviour == types.ElevatorDoorOpen)
-			
-			if elevator.Behaviour == types.ElevatorDoorOpen {
-				if hasAnyOrderAtFloor(elevator, elevator.CurrentFloor) {
-					doorOpenChan <- true  
-				}
-			} else if elevator.Behaviour == types.ElevatorIdle {
+			if elevator.Behaviour == types.ElevatorIdle {
 				if hasAnyOrderAtFloor(elevator, elevator.CurrentFloor) {
 					completedOrders := handleFloorArrival(&elevator, doorOpenChan, localLightsChan, elevator.MotorDirection)
 					sendElevatorUpdate(elevatorEvents, elevator, obstruction, completedOrders, nil)
