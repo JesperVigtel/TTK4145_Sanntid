24536264f2eef0ce326176744aada9b8bdec4f7e
Andreas Frafjord-Moe <andrfra@stud.ntnu.no>
2026-03-18 20:19:34 +0100

    kanskje fiks

--- elevator/internal/consensus/peer_state.go
+++ elevator/internal/consensus/peer_state.go
@@ -9,6 +9,30 @@ func isRemotePeerID(peerID int, selfID int) bool {
 	return peerID >= 0 && peerID < config.NElevators && peerID != selfID
 }
 
+// func updatePeerAvailability(
+// 	nodeRegistry types.GlobalNodeRegistry,
+// 	peerIsAlive [config.NElevators]bool,
+// 	orderTables [config.NElevators]types.OrderTable,
+// 	selfID int,
+// ) ([config.NElevators]bool, [config.NElevators]types.OrderTable) {
+// 	for _, lostPeerID := range nodeRegistry.Lost {
+// 		if !isRemotePeerID(lostPeerID, selfID) {
+// 			continue
+// 		}
+// 		peerIsAlive[lostPeerID] = false
+// 		orderTables = clearPeerHallOrders(orderTables, lostPeerID)
+// 	}
+
+// 	for _, newPeerID := range nodeRegistry.New {
+// 		if !isRemotePeerID(newPeerID, selfID) {
+// 			continue
+// 		}
+// 		peerIsAlive[newPeerID] = true
+// 		orderTables = clearPeerHallOrders(orderTables, newPeerID)
+// 	}
+// 	return peerIsAlive, orderTables
+// }
+
 func updatePeerAvailability(
 	nodeRegistry types.GlobalNodeRegistry,
 	peerIsAlive [config.NElevators]bool,
@@ -20,7 +44,7 @@ func updatePeerAvailability(
 			continue
 		}
 		peerIsAlive[lostPeerID] = false
-		orderTables = clearPeerHallOrders(orderTables, lostPeerID)
+		orderTables = clearAllOrdersForPeer(orderTables, lostPeerID)
 	}
 
 	for _, newPeerID := range nodeRegistry.New {
@@ -28,14 +52,38 @@ func updatePeerAvailability(
 			continue
 		}
 		peerIsAlive[newPeerID] = true
-		orderTables = clearPeerHallOrders(orderTables, newPeerID)
+		orderTables = clearAllOrdersForPeer(orderTables, newPeerID)
 	}
 	return peerIsAlive, orderTables
 }
 
+// func resetPeerObservations(
+// 	nodeRegistry types.GlobalNodeRegistry,
+// 	recordedElevatorStates [config.NElevators]types.HRAElevState,
+// 	peerOrderSnapshots [config.NElevators][config.NElevators]types.OrderTable,
+// 	selfID int,
+// ) ([config.NElevators]types.HRAElevState, [config.NElevators][config.NElevators]types.OrderTable) {
+// 	for _, lostPeerID := range nodeRegistry.Lost {
+// 		if !isRemotePeerID(lostPeerID, selfID) {
+// 			continue
+// 		}
+// 		recordedElevatorStates[lostPeerID] = types.HRAElevState{}
+// 		peerOrderSnapshots[lostPeerID] = [config.NElevators]types.OrderTable{}
+// 	}
+
+// 	for _, newPeerID := range nodeRegistry.New {
+// 		if !isRemotePeerID(newPeerID, selfID) {
+// 			continue
+// 		}
+// 		recordedElevatorStates[newPeerID] = types.HRAElevState{}
+// 		peerOrderSnapshots[newPeerID] = [config.NElevators]types.OrderTable{}
+// 	}
+// 	return recordedElevatorStates, peerOrderSnapshots
+// }
+
 func resetPeerObservations(
 	nodeRegistry types.GlobalNodeRegistry,
-	recordedElevatorStates [config.NElevators]types.HRAElevState,
+	lastPeerElevatorStates [config.NElevators]types.HRAElevState,
 	peerOrderSnapshots [config.NElevators][config.NElevators]types.OrderTable,
 	selfID int,
 ) ([config.NElevators]types.HRAElevState, [config.NElevators][config.NElevators]types.OrderTable) {
@@ -43,18 +91,26 @@ func resetPeerObservations(
 		if !isRemotePeerID(lostPeerID, selfID) {
 			continue
 		}
-		recordedElevatorStates[lostPeerID] = types.HRAElevState{}
+		lastPeerElevatorStates[lostPeerID] = types.HRAElevState{}
+
+		// Nullstill alt sendt FRA denne peeren
 		peerOrderSnapshots[lostPeerID] = [config.NElevators]types.OrderTable{}
+
+		// Nullstill alt andre peers husker OM denne peeren
+		peerOrderSnapshots = clearPeerSnapshotColumn(peerOrderSnapshots, lostPeerID)
 	}
 
 	for _, newPeerID := range nodeRegistry.New {
 		if !isRemotePeerID(newPeerID, selfID) {
 			continue
 		}
-		recordedElevatorStates[newPeerID] = types.HRAElevState{}
+		lastPeerElevatorStates[newPeerID] = types.HRAElevState{}
+
+		// Behandle restart/rejoin som en helt fersk instans
 		peerOrderSnapshots[newPeerID] = [config.NElevators]types.OrderTable{}
+		peerOrderSnapshots = clearPeerSnapshotColumn(peerOrderSnapshots, newPeerID)
 	}
-	return recordedElevatorStates, peerOrderSnapshots
+	return lastPeerElevatorStates, peerOrderSnapshots
 }
 
 func matchesLastPeerSnapshot(
@@ -113,4 +169,38 @@ func clearPeerHallOrders(
 		orderTables[peerID][floor][types.BtnHallDown] = types.OrderStandby
 	}
 	return orderTables
+}
+
+//
+
+func clearPeerCabOrders(
+	orderTables [config.NElevators]types.OrderTable,
+	peerID int,
+) [config.NElevators]types.OrderTable {
+	for floor := range config.NFloors {
+		orderTables[peerID][floor][types.BtnCab] = types.OrderStandby
+	}
+	return orderTables
+}
+
+func clearAllOrdersForPeer(
+	orderTables [config.NElevators]types.OrderTable,
+	peerID int,
+) [config.NElevators]types.OrderTable {
+	for floor := range config.NFloors {
+		orderTables[peerID][floor][types.BtnHallUp] = types.OrderStandby
+		orderTables[peerID][floor][types.BtnHallDown] = types.OrderStandby
+		orderTables[peerID][floor][types.BtnCab] = types.OrderStandby
+	}
+	return orderTables
+}
+
+func clearPeerSnapshotColumn(
+	peerOrderSnapshots [config.NElevators][config.NElevators]types.OrderTable,
+	ownerID int,
+) [config.NElevators][config.NElevators]types.OrderTable {
+	for peerID := range config.NElevators {
+		peerOrderSnapshots[peerID][ownerID] = types.OrderTable{}
+	}
+	return peerOrderSnapshots
 }
